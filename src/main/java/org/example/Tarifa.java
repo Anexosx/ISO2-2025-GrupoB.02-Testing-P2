c1f4d7032a20437cc9e5a0f2757b2fc5407906d3
Blancaamg1 <blanca.alanon@alu.uclm.es>
2025-12-04 20:01:54 +0100

    Código problema 2

--- src/main/java/org/example/Tarifa.java
+++ src/main/java/org/example/Tarifa.java
@@ -5,18 +5,10 @@ class Tarifa {
         String tarifa = "Sin tarifa"; //inicializacion de la tarifa por si no se cumple ningun tipo de condicion de tarifa
         double precioFinal = precioVuelo * numBilletes;
 
-        /* 1- Pajarillo: Si el cliente es menor de edad, y realiza al menos 6 vuelos al año, el tipo de tarifa que se
-           le puede ofrecer es “Pajarillo” (con un descuento de 10% sobre el precio del vuelo). */
-
         if (edad < 18 && numeroVuelos >= 6) {
             tarifa = "Pajarillo";
             precioFinal = calcularPrecioVuelo(tarifa, precioVuelo, numBilletes);
         }
-
-        /* 2- Gorrión:  Si el cliente tiene entre 18 y 25 años y está estudiando en una universidad en otra
-           ciudad, desplazándose en clase turista del domicilio familiar al menos una vez al mes
-           durante los meses del curso entre ambas ciudades, se le ofrecerá la tarifa “Gorrión” (con
-           un descuento del 15% sobre el precio del vuelo).  */
 
         if ((edad >= 18 && edad <= 25) && esEstudiante && tipoClase.equals("turista") && numDesplazamientosMes >= 1) {
 
@@ -24,11 +16,6 @@ class Tarifa {
             precioFinal = calcularPrecioVuelo(tarifa, precioVuelo, numBilletes);
         }
 
-        /* 3- Viaja ahora que puedes / Atreviéndose a saltar del Nido: Si el cliente tiene entre 18 y 25 años, y si ha empezado a trabajar, pero vive aun con sus
-           padres, y realiza al menos tres viajes de placer al año en clase turista se le ofrecerá la
-           tarifa “Viaja ahora que puedes” (con un descuento del 5% sobre el precio del vuelo); por
-           el contrario, si ya no vive con sus padres, se le ofrecerá la tarifa “Atreviéndose a saltar
-           del Nido” (con un descuento del 25% sobre el precio del vuelo).  */
 
         if (edad >= 18 && edad <= 25 && trabaja && numeroVuelos >= 3 && tipoClase.equals("turista")) {
 
@@ -43,12 +30,6 @@ class Tarifa {
             }
         }
 
-        /* 4- Conoce Europa (sin o con niños): Si el cliente tiene entre 18 y 25 años, y si ha empezado a trabajar, pero vive aun con sus
-           padres, y realiza al menos tres viajes de placer al año en clase turista se le ofrecerá la
-           tarifa “Viaja ahora que puedes” (con un descuento del 5% sobre el precio del vuelo); por
-           el contrario, si ya no vive con sus padres, se le ofrecerá la tarifa “Atreviéndose a saltar
-           del Nido” (con un descuento del 25% sobre el precio del vuelo).  */
-
         if (edad > 25 && ingresos > 20000 && ingresos < 35000 && numeroVuelos >= 6 && tipoClase.equals("turista") && destino.equals("Europa")) {
 
             if (!viajaConNinos) {
@@ -62,12 +43,6 @@ class Tarifa {
             }
         }
 
-        /* 5- Conoce el Mundo (sin o con niños): Si es mayor de 25 años, tiene unos ingresos superiores a 20.000 € pero menores que
-              35.000 y realiza al menos 6 viajes al año en clase turista a destinos dentro de Europa se
-              le puede ofrecer la tarifa “Conoce Europa” (con un descuento del 15% sobre el precio
-              del vuelo) y si viaja con niños (menores de 12 años), se le puede ofrecer la tarifa “Conoce
-              Europa con tus peques” (con un descuento del 10% sobre el precio de cada uno de los
-              billetes).  */
 
         if (edad > 25 && ingresos >= 35000 && numeroVuelos >= 6 &&
                 tipoClase.equals("business") &&
